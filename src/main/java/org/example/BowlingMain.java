5d6f980692bb2301bd64434f01e4eec67448fc6e
Alpár Imets <16115559+alparimets@users.noreply.github.com>
2025-06-14 11:43:02 +0200

    remove double folder

---
+++ src/main/java/org/example/BowlingMain.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
+import org.example.service.GameService;
+import org.example.service.PlayerService;
+import org.example.service.PrintService;
+
+@QuarkusMain
+public class BowlingMain implements QuarkusApplication {
+  @Inject PrintService printService;
+  @Inject PlayerService playerService;
+  @Inject GameService gameService;
+
+  @Override
+  public int run(String... args) throws Exception {
+    printService.printWelcomeMessage();
+    var players = playerService.getPlayers();
+    printService.printPlayerNames(players);
+    gameService.startGame(players);
+
+    return 0;
+  }
+}
